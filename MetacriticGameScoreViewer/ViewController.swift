4bd699236b02cd4117ad7a2e119881690b2f2d69
김재구 <rlaworn1993@gmail.com>
2020-03-17 19:37:18 +0900

    init

---
+++ MetacriticGameScoreViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MetacriticGameScoreViewer
+//
+//  Created by 김재구 on 2020/03/17.
+//  Copyright © 2020 jaeguKim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
