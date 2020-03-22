361174c44f4f62e839411750ed182b0eca3b6270
김재구 <rlaworn1993@gmail.com>
2020-03-22 17:30:30 +0900

    completed register/login function

--- MetacriticGameScoreViewer/Controller/RegisterViewController.swift
+++ MetacriticGameScoreViewer/Controller/RegisterViewController.swift
@@ -8,6 +8,8 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    @IBAction func registerBtnPressed(_ sender: UIButton) {
         if let email = emailTextField.text, let password = passwordTextField.text {
             Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
                 if let e = error {
