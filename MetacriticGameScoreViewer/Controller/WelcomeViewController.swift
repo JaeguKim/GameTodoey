f3f1326e85be7fb2476f7af6e312f3d1effa3a32
김재구 <rlaworn1993@gmail.com>
2020-03-22 14:30:38 +0900

    Added WelcomeViewController

---
+++ MetacriticGameScoreViewer/Controller/WelcomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeViewController.swift
+//  MetacriticGameScoreViewer
+//
+//  Created by 김재구 on 2020/03/22.
+//  Copyright © 2020 jaeguKim. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
