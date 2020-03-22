89e59db943b521a66f43ab96b2e32e257d3e3c95
김재구 <rlaworn1993@gmail.com>
2020-03-22 19:30:03 +0900

    added alert when logging in, registering

--- MetacriticGameScoreViewer/Controller/LoginViewController.swift
+++ MetacriticGameScoreViewer/Controller/LoginViewController.swift
@@ -13,10 +13,20 @@ class LoginViewController: UIViewController {
     
     @IBAction func loginBtnPressed(_ sender: UIButton) {
         if let email = emailTextField.text, let password = passwordTextField.text {
+            let alert = UIAlertController(title: "Loggin in...", message: "", preferredStyle: .alert)
+            self.present(alert, animated: true)
             Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
                 if let e = error {
-                    print(e)
+                    print(e.localizedDescription)
+                    alert.dismiss(animated: true, completion: nil)
+                    let alert = UIAlertController(title: "Failed logging in", message: "", preferredStyle: .alert)
+                    self.present(alert, animated: true) {
+                       DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                           alert.dismiss(animated: true, completion: nil)
+                        }
+                    }
                 } else {
+                    alert.dismiss(animated: true, completion: nil)
                     self.performSegue(withIdentifier: Const.loginSegue, sender: self)
                 }
             }
--- MetacriticGameScoreViewer/Controller/RegisterViewController.swift
+++ MetacriticGameScoreViewer/Controller/RegisterViewController.swift
@@ -11,11 +11,21 @@ class RegisterViewController: UIViewController {
     }
     @IBAction func registerBtnPressed(_ sender: UIButton) {
         if let email = emailTextField.text, let password = passwordTextField.text {
+            let alert = UIAlertController(title: "Registering...", message: "", preferredStyle: .alert)
+            self.present(alert, animated: true)
             Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
                 if let e = error {
                     print(e.localizedDescription)
+                    alert.dismiss(animated: true, completion: nil)
+                    let alert = UIAlertController(title: "Failed Registering", message: "", preferredStyle: .alert)
+                    self.present(alert, animated: true) {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                            alert.dismiss(animated: true, completion: nil)
+                        }
+                    }
                 }
                 else {
+                    alert.dismiss(animated: true, completion: nil)
                     self.performSegue(withIdentifier: Const.registerSegue, sender: self)
                 }
             }
