bf57e6169fd6d2b8d0fae49419cd16225e1497f1
김재구 <rlaworn1993@gmail.com>
2020-03-29 10:58:33 +0900

    added Realm Manager

--- MetacriticGameScoreViewer/Controller/LibraryViewController.swift
+++ MetacriticGameScoreViewer/Controller/LibraryViewController.swift
@@ -1,25 +1,32 @@
 import UIKit
-import RealmSwift
 import SwipeCellKit
+import RealmSwift
 
 class LibraryViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-    var realm = try! Realm()
+  
     var libraryInfoList : Results<LibraryInfo>?
     var selectedLibrary : LibraryInfo?
+    let realmManager = RealmManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         parent?.navigationItem.title = "Library"
+        navigationController?.setNavigationBarHidden(false, animated: true)
         tableView.dataSource = self
         tableView.delegate = self
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 150
         tableView.register(UINib(nibName: Const.LibraryCellNibName, bundle: nil), forCellReuseIdentifier: Const.libraryCellIdentifier)
-        loadLibraries()
+        libraryInfoList = realmManager.loadLibraries()
+        tableView.reloadData()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+
     @IBAction func addBtnPressed(_ sender: UIBarButtonItem) {
         var textField = UITextField()
         let alert = UIAlertController(title: "New Library", message: "Enter a name for this library", preferredStyle: .alert)
@@ -33,7 +40,7 @@ class LibraryViewController: UIViewController {
         let saveAction = UIAlertAction(title: "Save", style: .default) { (action) in
             let newLibrary = LibraryInfo()
             newLibrary.libraryTitle = textField.text!
-            self.save(realmObj: newLibrary)
+            self.realmManager.save(realmObj: newLibrary)
             self.tableView.reloadData()
         }
         alert.addAction(cancelAction)
@@ -41,33 +48,7 @@ class LibraryViewController: UIViewController {
         present(alert,animated: true, completion: nil)
     }
 
-    func loadLibraries() {
-        libraryInfoList = realm.objects(LibraryInfo.self)
-        tableView.reloadData()
-    }
-    
-    func save(realmObj : LibraryInfo) {
-        do {
-            try realm.write {
-                realm.add(realmObj)
-            }
-        } catch {
-            print("Error Saving context \(error)")
-        }
-    }
-    
-    func updateModel(at indexPath: IndexPath) {
-          if let itemForDeletion = self.libraryInfoList?[indexPath.row]
-          {
-              do {
-                  try self.realm.write() {
-                      self.realm.delete(itemForDeletion)
-                  }
-              } catch {
-                  print("Error occurred when deleting item \(error)")
-              }
-          }
-      }
+  
     
     func showAlertMessage(title : String) {
         let alert = UIAlertController(title: title, message: "", preferredStyle: .alert)
@@ -123,7 +104,9 @@ extension LibraryViewController : SwipeTableViewCellDelegate {
     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
         guard orientation == .right else {return nil}
         let deleteAction = SwipeAction(style: .destructive, title: "Delete") { (action, indexPath) in
-            self.updateModel(at: indexPath)
+            if let libraryInfo = self.libraryInfoList?[indexPath.row] {
+                self.realmManager.deleteLibrary(libraryInfo: libraryInfo)
+            }
         }
         deleteAction.image = UIImage(named: "delete-icon")
         return [deleteAction]
---
+++ MetacriticGameScoreViewer/Model/RealmManager.swift
@@ -0,0 +1,40 @@
+//
+//  RealmManager.swift
+//  MetacriticGameScoreViewer
+//
+//  Created by 김재구 on 2020/03/28.
+//  Copyright © 2020 jaeguKim. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+struct RealmManager {
+    var realm = try! Realm()
+    
+    func loadLibraries() -> Results<LibraryInfo> {
+        //  libraryInfoList = realm.objects(LibraryInfo.self)
+         //
+        return realm.objects(LibraryInfo.self)
+      }
+      
+      func save(realmObj : LibraryInfo) {
+          do {
+              try realm.write {
+                  realm.add(realmObj)
+              }
+          } catch {
+              print("Error Saving context \(error)")
+          }
+      }
+      
+    func deleteLibrary(libraryInfo : LibraryInfo) {
+        do {
+            try self.realm.write() {
+                self.realm.delete(libraryInfo)
+            }
+        } catch {
+            print("Error occurred when deleting library \(error)")
+        }
+    }
+}
